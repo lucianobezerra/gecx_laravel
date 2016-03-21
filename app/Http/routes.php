f377e3dc034a4eff8a6d3cfe5d5e866c119d3d38
Luciano Bezerra <luciano@grupotubform.com.br>
2016-03-21 18:06:44 -0300

    ajustes nas rotas

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -7,6 +7,7 @@ Route::group(['middleware' => 'web'], function () {
     Route::auth();
 
     Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function(){
+      Route::get('/',           ['uses' => 'HomeController@index', 'role' => 'admin_system_view']);
       Route::get('system',      ['uses' => 'SystemController@index', 'role' => 'admin_system_view']);
       Route::get('system/edit', ['uses' => 'SystemController@index', 'role' => 'admin_system_edit']);
     });
